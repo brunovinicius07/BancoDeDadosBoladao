468c8b6759c33880181a4655614dff9e7191b911
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-03-08 14:43:32 -0300

    Created 01 SelectExemplo

---
+++ 01  SelectExemplo.sql
@@ -0,0 +1,11 @@
+
+        SELECT BusinessEntityID,
+                        PersonType,
+                        Title,
+                        FirstName,
+                        MiddleName,
+                        LastName,
+                        ModifiedDate
+            FROM Person.Person 
+         WHERE FirstName     = 'Ken'
+               AND MiddleName = 'L'
