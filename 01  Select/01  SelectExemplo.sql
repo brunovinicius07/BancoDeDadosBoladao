e1cc6584b49cee2455ca0769998ed5929974e294
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-03-09 12:34:14 -0300

    Refactored Select

---
+++ 01  Select/01  SelectExemplo.sql
@@ -0,0 +1,27 @@
+  
+        SELECT BusinessEntityID,
+                        PersonType,
+                        Title,
+                        FirstName,
+                        MiddleName,
+                        LastName,
+                        ModifiedDate
+            FROM Person.Person 
+         WHERE FirstName     = 'Ken'
+               AND MiddleName = 'L'
+
+
+      SELECT TOP 1000
+             BusinessEntityID AS [ID PESSOA],
+             PersonType       AS [TIPO PESSOA],
+             Title            AS [TITULO],
+             FirstName        AS [PRIMEIRO NOME],
+             MiddleName       AS [NOME DO MEIO],
+             LastName         AS [ULTIMO NOME]
+        FROM Person.Person
+       ORDER BY FirstName
+
+
+       SELECT DISTINCT
+              FirstName
+         FROM Person.Person
---
+++ 01  Select/02 SelectExemplo.sql
@@ -0,0 +1,31 @@
+
+        SELECT 
+                        BusinessEntityID,
+                        PersonType,
+                        Title,
+                        FirstName,
+                        MiddleName,
+                        LastName,
+                        ModifiedDate
+
+           FROM Person.Person
+         ORDER BY BusinessEntityID
+         OFFSET 5 ROWS     
+
+
+          SELECT 
+                        BusinessEntityID,
+                        PersonType,
+                        Title,
+                        FirstName,
+                        MiddleName,
+                        LastName,
+                        ModifiedDate
+
+           FROM Person.Person
+         ORDER BY BusinessEntityID
+         OFFSET 5 ROWS
+           FETCH NEXT 10 ROWS ONLY                 
+                        
+
+
