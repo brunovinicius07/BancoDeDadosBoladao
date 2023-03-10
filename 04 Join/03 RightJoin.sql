9817c74876cb0683183be6a44727da195ded4086
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-03-10 16:10:25 -0300

    Created 03 RightJoinExemplo

---
+++ 04 Join/03 RightJoin.sql
@@ -0,0 +1,14 @@
+
+        SELECT SSP.BusinessEntityID AS [ID.SSP],
+                        PP.BusinessEntityID   AS [ID.PP], 
+                        TerritoryID,
+                        SalesQuota,
+                        Bonus,
+                        SalesLastYear,
+                        FirstName,
+                        MiddleName,
+                        SalesLastYear
+                        
+            FROM Sales.SalesPerson AS SSP
+           RIGHT JOIN  Person.Person AS PP
+                 ON  SSP.BusinessEntityID = PP.BusinessEntityID
